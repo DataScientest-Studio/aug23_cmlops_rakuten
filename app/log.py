b3dd96a793ade529f10b822aa264a30497002f25
Flav63s <135693215+Flav63s@users.noreply.github.com>
2023-11-03 12:06:50 +0100

    Delete app directory

--- app/log.py
+++
@@ -1,7 +0,0 @@
-# log.py
-import logging
-
-# Configurer le logger
-logging.basicConfig(filename='api_log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
-
-logger = logging.getLogger(__name__)
