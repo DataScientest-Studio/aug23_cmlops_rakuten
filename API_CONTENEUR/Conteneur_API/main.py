01148a958911d587048bb0f7bc675f9cbdf40353
MichaelD24 <122894540+MichaelD24@users.noreply.github.com>
2023-11-28 16:56:27 +0100

    Update main.py
    Intégration dag dans fichier main.py

--- API_CONTENEUR/Conteneur_API/main.py
+++ API_CONTENEUR/Conteneur_API/main.py
@@ -94,5 +94,13 @@ async def predict_endpoint(token: str = Depends(get_token), text: str = Form(...
     else:
         raise HTTPException(status_code=400, detail="Image file is missing")
 
-
+    execution_date = datetime.utcnow()
+    dag_trigger_response = client.trigger_dag(
+        dag_id='nom_de_votre_dag',
+        run_id=f"predict_run_{execution_date}",
+        conf={'text': text, 'image_path': temp_image_path, 'token': token},
+        execution_date=execution_date,
+    )
+    
+    return JSONResponse(content={"message": "Prédiction réussie. DAG déclenché."})
 
