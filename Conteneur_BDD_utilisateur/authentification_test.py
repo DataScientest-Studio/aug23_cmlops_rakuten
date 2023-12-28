fc45e94adbcf2e34ce357458f8f491b728bb4cdd
Flav63s <135693215+Flav63s@users.noreply.github.com>
2023-12-28 09:29:41 +0100

    Delete Conteneur_BDD_utilisateur directory

--- Conteneur_BDD_utilisateur/authentification_test.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from authentification import app
-
-client = TestClient(app)
-
-def test_authenticate():
-    response = client.post("/auth", auth=("test", "secret63"))
-    assert response.status_code == 200
-    assert "token" in response.json()
