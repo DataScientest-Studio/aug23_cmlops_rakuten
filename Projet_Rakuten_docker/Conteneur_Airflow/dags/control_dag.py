966d9b5d73ace2e72ad726290fe777183ce5b059
MichaelD24 <122894540+MichaelD24@users.noreply.github.com>
2024-01-08 09:51:21 +0100

    Update control_dag.py
    ajout docstrings

--- Projet_Rakuten_docker/Conteneur_Airflow/dags/control_dag.py
+++ Projet_Rakuten_docker/Conteneur_Airflow/dags/control_dag.py
@@ -3,6 +3,34 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
+"""
+DAG de test pour l'authentification
+
+Ce DAG, nommé 'test_dag', a été conçu pour effectuer un test d'authentification en exécutant
+le script 'authentification_test.py' situé dans '/app/data2/'. Il utilise Apache Airflow pour
+planifier et exécuter cette tâche de test.
+
+Arguments par défaut :
+    owner (str): Propriétaire du DAG.
+    depends_on_past (bool): Indique si une tâche doit dépendre de l'exécution réussie de la précédente.
+    start_date (datetime): Date de début d'exécution du DAG.
+    email_on_failure (bool): Indique si les notifications par e-mail doivent être envoyées en cas d'échec.
+    email_on_retry (bool): Indique si les notifications par e-mail doivent être envoyées lors d'une tentative de réexécution.
+    retries (int): Nombre de tentatives de réexécution en cas d'échec d'une tâche.
+    retry_delay (timedelta): Délai entre les tentatives de réexécution.
+
+Tâches :
+    - run_authentification_test: Tâche Python exécutant le script 'authentification_test.py'. Si une erreur
+      se produit lors de l'exécution, une exception est levée.
+    - end_task: Tâche factice indiquant la fin du DAG.
+
+Note :
+    - Assurez-vous que le script 'authentification_test.py' est présent dans le répertoire '/app/data2/'.
+    - La planification (schedule_interval) est définie sur None, ce qui signifie que le DAG doit être déclenché manuellement.
+    - Les notifications par e-mail en cas d'échec sont désactivées (email_on_failure=False).
+    - Ce DAG effectue un simple test d'authentification et peut être adapté en fonction des besoins.
+"""
+
 # Définir les arguments par défaut pour le DAG
 default_args = {
     'owner': 'admin',
